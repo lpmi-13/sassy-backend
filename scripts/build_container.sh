1fe96dae66174c9ae3e2dcfedfe1fd3d994ae5c5
Adam Leskis <leskis@gmail.com>
2024-04-27 16:36:26 +0100

    also build the db-seed container for local testing

--- scripts/build_container.sh
+++ scripts/build_container.sh
@@ -1,3 +1,7 @@
 docker build -t sassy-backend .
 docker tag sassy-backend localhost:5001/sassy-backend
 docker push localhost:5001/sassy-backend
+
+docker build -t sassy-backend-db-seed -f db-seed/Dockerfile db-seed/
+docker tag sassy-backend-db-seed localhost:5001/sassy-backend-db-seed
+docker push localhost:5001/sassy-backend-db-seed
