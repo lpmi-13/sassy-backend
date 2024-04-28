e78ed7539c6712de5e62155434ccd513334d36d6
Adam Leskis <leskis@gmail.com>
2024-04-29 00:07:48 +0100

    remove id from the model

--- backend/sassy/models.py
+++ backend/sassy/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class SassInfo(models.Model):
-    id = models.IntegerField(primary_key=True)
     employee_number = models.IntegerField()
     founder = models.CharField(max_length=50)
     funding = models.IntegerField()
